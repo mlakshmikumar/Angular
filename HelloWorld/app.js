1b0e3f37c86887a67db115e2cfff2a9d2155ffca
mlakshmikumar <lmadhavan@ancestry.com>
2014-02-26 22:52:53 -0800

    Commit Test
    Trying first commit into github

---
+++ HelloWorld/app.js
@@ -0,0 +1,4 @@
+/**
+ * Created by arun on 2/26/14.
+ */
+var app = angular.module('MyTutorialApp',[]);
---
+++ HelloWorld/maincontroller.js
@@ -0,0 +1,4 @@
+app.controller('MainController', function($scope){
+   $scope.hello = "First Angular";
+   $scope.Names = ['First','Second','Third'];
+});
